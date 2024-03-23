86ce4823d58075ba7f3fdfbb5fd8dbca2ef615af
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-03-23 08:48:26 +0800

    Initial commit

---
+++ src/components/FlowChart.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1> This is a test to see how it works</h1>
+  <p> I've never done this before</p>
+</template>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { createPinia } from "pinia";
+import "./style.css";
+
+// Import your components
+import HelloWorld from "./components/HelloWorld.vue";
+import FlowChart from "./components/FlowChart.vue";
+
+// Create a new router instance
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: HelloWorld },
+    { path: "/canvas", component: FlowChart },
+  ],
+});
+
+// Create pinia instance
+const pinia = createPinia();
+// Create the Vue app instance
+const app = createApp(App);
+
+// Use the router in your Vue app
+app.use(router);
+
+app.use(pinia);
+
+// Mount the app
+app.mount("#app");
