8c4ff198ab85efe0c79a04b948b4a91b8731c7cb
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-03-24 15:18:28 +0800

    Handle form submission

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,6 +5,11 @@ import { ref } from 'vue';
 import { useNodeStore } from '../stores/useNodeStore';
 
 const dialogElement = ref(null);
+const formData = ref({
+  title: '',
+  type: '',
+  description: ''
+});
 
 const closeDialog = () => {
   dialogElement.value.close();
@@ -14,10 +19,20 @@ const store = useNodeStore()
 const { addNode } = store;
 
 const createNewNode = () => {
-  // get form data
-  addNode(data)
+  // Access form data
+  const data = { ...formData.value };
+  // Add the node using the store
+  addNode(data);
+  // Close the dialog
+  closeDialog();
+  formData.value = {
+    title: '',
+    description: '',
+    type: ''
+  }
 }
 </script>
+
 <template>
   <div class="grid grid-rows-[5fr_1fr]">
     <FlowChart />
@@ -25,19 +40,18 @@ const createNewNode = () => {
     <dialog ref="dialogElement" class="w-1/2 h-1/2 rounded-md p-4">
       <div class="grid grid-rows-[1fr_3fr] h-full">
         <h2>Create New Node</h2>
-        <form class="grid grid-rows gap-2">
+        <form @submit.prevent="createNewNode" class="grid grid-rows gap-2">
           <div class="grid grid-cols-2 gap-2">
-
-            <input placeholder="Title" class="h-1/2 p-4" name="title" />
-            <select class="h-1/2 p-4" name="type">
+            <input v-model="formData.title" placeholder="Title" class="h-1/2 p-4" name="title" />
+            <select v-model="formData.type" class="h-1/2 p-4" name="type">
               <option value="sendMessage">sendMessage</option>
               <option value="addComment">addComment</option>
               <option value="businessHours">businessHours</option>
             </select>
           </div>
-          <textarea placeholder="Description" class="p-4" name="description" />
+          <textarea v-model="formData.description" placeholder="Description" class="p-4" name="description" />
           <div class=" grid grid-cols-2 gap-2">
-            <button type="submit" @click="createNewNode">Create</button>
+            <button type="submit">Create</button>
             <button type="button" @click="closeDialog">Close</button>
           </div>
         </form>
